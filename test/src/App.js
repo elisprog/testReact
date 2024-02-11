3eb38d318a722b81ba2825f85e58dcbc97180fc8
Elizaveta <bogdanovaelis@gmail.com>
2024-02-11 20:43:19 +0300

    second commit

--- test/src/App.js
+++ test/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+export const data = [
+  ["Activity", 'Hours'],
+  ["Работа", 7],
+  ["Еда и сон", 11],
+  ["Терапия и отдых", 3],
+  ["Учёба", 2],
+  ["Душ", 1],
+];
+export const options = {
+  title: "Жизнь по часам в процентах",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+       <h1>Привет, вот моя жизнь:</h1>
+<Chart 
+chartType="PieChart"
+data={data}
+options={options}
+width={"100%"}
+height={"400px"}
+/>
       </header>
     </div>
   );
